ae32558290e6180d84ffa0b3088133677b836b43
Davor Rilko <drilko@span.eu>
2020-09-10 12:00:53 +0200

    nekaj random

--- web/src/app/app.component.ts
+++ web/src/app/app.component.ts
@@ -1,10 +1,18 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css'],
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'code-convention-poc';
+
+  ngOnInit() {
+    let pet = 2;
+    pet += 3;
+    if (pet === 8) {
+      console.error('ok');
+    }
+  }
 }
